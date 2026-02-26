1beb2353cf1c97fddcb9794529fd222eecf17901
anjanadc06 <anjana14425@gmail.com>
2026-02-26 16:06:32 +0530

    [Anjana] Added [UC1]

---
+++ App/src/PalindromeCheckerApp.java
@@ -0,0 +1,7 @@
+public class PalindromeCheckerApp {
+    public static void main(String[] args){
+        System.out.println("Welcome to Panlindrome Checker App Management system");
+        System.out.println("Version: 1.0");
+        System.out.println("system initialized successfully");
+    }
+}
